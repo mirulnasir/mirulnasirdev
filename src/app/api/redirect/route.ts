aff3a1b6c301c3167ada29e64b257033c7f31014
Mohd-Amirul-Asraf-Bin-Mohamad-Nasir <amirulasraf.binmohamadnasir@etiqa.com.my>
2025-01-06 11:43:39 +0800

    feat: fix redirect

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -5,6 +5,5 @@ const defaultUrl = 'https://google.com'
 export async function POST(request: NextRequest) {
     const toUrl = request.nextUrl.searchParams.get('to')
     const redirectUrl = toUrl || defaultUrl
-    const response = NextResponse
-    return response.redirect(redirectUrl)
+    return Response.redirect(redirectUrl)
 }
